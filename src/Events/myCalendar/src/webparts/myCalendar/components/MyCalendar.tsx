93efbff6ff4602bbca358ac4901971c7b8f8c0d9
Derek Smay <derek.smay@microsoft.com>
2021-04-24 13:43:20 -0400

    Initial changes for Panel

--- src/Events/myCalendar/src/webparts/myCalendar/components/MyCalendar.tsx
+++ src/Events/myCalendar/src/webparts/myCalendar/components/MyCalendar.tsx
@@ -8,11 +8,16 @@ import { IMyCalendarState } from './IMyCalendarState';
 import { Providers } from '@microsoft/mgt';
 import { Agenda, MgtTemplateProps } from '@microsoft/mgt-react';
 import { WebPartTitle } from '@pnp/spfx-controls-react/lib/WebPartTitle';
+import { CommandBar, ICommandBarItemProps } from 'office-ui-fabric-react/lib/CommandBar';
 import { Link } from 'office-ui-fabric-react/lib/components/Link';
 import { Event } from '@microsoft/microsoft-graph-types';
 import { PrimaryButton, IIconProps } from 'office-ui-fabric-react';
 import { initializeIcons } from '@uifabric/icons';
 import { mergeStyles, mergeStyleSets } from 'office-ui-fabric-react/lib/Styling';
+import { Panel, PanelType } from 'office-ui-fabric-react/lib/Panel';
+import { Stack, IStackStyles, IStackTokens, IStackItemStyles } from 'office-ui-fabric-react/lib/Stack';
+import { Text } from 'office-ui-fabric-react/lib/Text';
+
 
 const EventInfo = (props: MgtTemplateProps) => {
   /**
@@ -58,7 +63,7 @@ const EventInfo = (props: MgtTemplateProps) => {
   const minutes: number = startTime.getMinutes();
 
   return <div className={`${styles.meetingWrapper} ${event.showAs}`}>
-    <Link href={event.webLink} className={styles.meeting} target='_blank'>
+    <Link className={styles.meeting} onClick={() => this._showEventDetails(event.id) } >
       <div className={styles.linkWrapper}>
         <div className={styles.start}>{`${startTime.getHours()}:${minutes < 10 ? '0' + minutes : minutes}`}</div>
         <div>
@@ -89,7 +94,9 @@ export default class MyCalendar extends React.Component<IMyCalendarProps, IMyCal
     this.state = {
       error: undefined,
       loading: true,
-      renderedDateTime: new Date()
+      renderedDateTime: new Date(),
+      isOpen: false,
+      activeEvent: undefined
     };
   }
 
@@ -168,6 +175,32 @@ export default class MyCalendar extends React.Component<IMyCalendarProps, IMyCal
     }
   }
 
+  private _showEventDetails = (messageId: string):void => {
+    this 
+      ._getEventDetails(messageId)
+      .then((activeEvent:Event):void =>{
+        this.setState({
+          isOpen:true,
+          activeEvent:activeEvent
+        });
+      }); 
+  }
+
+  private _getEventDetails = (messageId: string): Promise<Event> => {    
+    return new Promise<Event>((resolve, reject) => {
+      Providers.globalProvider.graph
+        // get the mailbox settings
+        .api(`me/mailboxSettings/calendar/events/`+ messageId)
+        .version("v1.0")
+        .get((err: any, res: Event): void => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(res);
+        });
+    });
+  }
+
   public render(): React.ReactElement<IMyCalendarProps> {
     const date: Date = new Date();
     const now: string = date.toISOString();
@@ -177,6 +210,24 @@ export default class MyCalendar extends React.Component<IMyCalendarProps, IMyCal
     date.setUTCSeconds(0);
     date.setDate(date.getDate() + (this.props.daysInAdvance || 0));
     const midnight: string = date.toISOString();
+
+    const recipientStackTokens: IStackTokens = {
+      childrenGap: 7    
+    };
+  
+    const messageDetailsStackTokens: IStackTokens = {
+      childrenGap: 3    
+    };
+
+    const messageDetailsCommandBarFarItems: ICommandBarItemProps[] = [      
+      {
+        key: 'viewInToDo',
+        text: 'View in ToDo',
+        iconProps: { iconName: 'OpenInNewWindow' },
+        href: this.state.activeEvent.webLink,
+        target: '_blank'
+      }
+    ];
 
     return (
       <div className={styles.myCalendar}>
@@ -193,13 +244,41 @@ export default class MyCalendar extends React.Component<IMyCalendarProps, IMyCal
               <Agenda
                 preferredTimezone={this.state.timeZone}
                 eventQuery={`me/calendar/calendarView?startDateTime=${now}&endDateTime=${midnight}`}
-                showMax={this.props.numMeetings > 0 ? this.props.numMeetings : undefined}>
-                <EventInfo template='event' />
+                showMax={this.props.numMeetings > 0 ? this.props.numMeetings : undefined} >
+                <EventInfo template='event'  />
               </Agenda>
             </div>
             <Link href='https://outlook.office.com/owa/?path=/calendar/view/Day' target='_blank'>{strings.ViewAll}</Link>
           </>
         }
+            {
+          (true && this.state.isOpen) ? 
+        <Panel
+          //className={styles.messageDetails}
+          isLightDismiss          
+          isOpen={this.state.isOpen}
+          onDismiss={() => { this.setState( { isOpen: false }); } }
+          type={PanelType.largeFixed}          
+          closeButtonAriaLabel="Close"
+          headerText={this.state.activeEvent.subject}
+        >   
+          <Stack tokens={messageDetailsStackTokens}>            
+            <CommandBar
+              items={[]}
+              farItems={messageDetailsCommandBarFarItems}
+              //className={styles.commandBar}
+              ariaLabel="Use left and right arrow keys to navigate between commands"
+            />
+            <Text>
+              {this.state.activeEvent.subject}
+            </Text>          
+           {  (this.state.activeEvent.body.contentType === "html") ?           
+            <div dangerouslySetInnerHTML={{ __html: this.state.activeEvent.body.content }}></div> : 
+            <div>{this.state.activeEvent.body.content}</div>
+            }        
+          </Stack>    
+        </Panel> : null
+      }
         {
           !this.state.loading &&
           this.state.error &&
