74dd53750b36dcff354fd8863906624705ff0045
Derek Smay <derek.smay@microsoft.com>
2021-04-27 19:23:26 -0400

    remove MGT component

--- src/Events/myCalendar/src/webparts/myCalendar/components/IMyCalendarProps.ts
+++ src/Events/myCalendar/src/webparts/myCalendar/components/IMyCalendarProps.ts
@@ -2,9 +2,10 @@
 // Licensed under the MIT license.
 import { IMyCalendarWebPartProps } from "../MyCalendarWebPart";
 import { DisplayMode } from "@microsoft/sp-core-library";
-
+import { MSGraphClient } from "@microsoft/sp-http";
 
 export interface IMyCalendarProps extends IMyCalendarWebPartProps {
   displayMode: DisplayMode;
+  graphClient: MSGraphClient;
   updateProperty: (value: string) => void;
 }
