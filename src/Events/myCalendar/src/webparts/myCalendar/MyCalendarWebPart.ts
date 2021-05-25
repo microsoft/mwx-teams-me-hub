007ea5987bad14ea5e0a17ba48fbdc174d00b8c7
John Scott (*) <goodjohnscott@outlook.com>
2021-05-25 16:22:43 -0400

    In progress of adding the web part title properties

--- src/Events/myCalendar/src/webparts/myCalendar/MyCalendarWebPart.ts
+++ src/Events/myCalendar/src/webparts/myCalendar/MyCalendarWebPart.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
+import { HeaderDisplay, ClickAction } from './enums';
 import { Version } from '@microsoft/sp-core-library';
 import {
   BaseClientSideWebPart
@@ -9,7 +10,9 @@ import {
 import {
   IPropertyPaneConfiguration,
   PropertyPaneTextField,
-  PropertyPaneSlider
+  PropertyPaneSlider,
+  PropertyPaneDropdown,
+  PropertyPaneToggle
 } from '@microsoft/sp-property-pane';
 
 import * as strings from 'MyCalendarWebPartStrings';
@@ -23,6 +26,10 @@ export interface IMyCalendarWebPartProps {
   refreshInterval: number;
   daysInAdvance: number;
   numMeetings: number;
+  clickAction: ClickAction;
+  showNew: boolean;
+  showViewAll: boolean;
+  headerDisplay?: HeaderDisplay;
 }
 
 export default class MyCalendarWebPart extends BaseClientSideWebPart<IMyCalendarWebPartProps> {
@@ -48,6 +55,10 @@ export default class MyCalendarWebPart extends BaseClientSideWebPart<IMyCalendar
         refreshInterval: this.properties.refreshInterval,
         daysInAdvance: this.properties.daysInAdvance,
         numMeetings: this.properties.numMeetings,
+        clickAction: this.properties.clickAction,
+        showNew: this.properties.showNew,
+        showViewAll: this.properties.showViewAll,
+        headerDisplay: this.properties.headerDisplay,
         // pass the current display mode to determine if the title should be
         // editable or not
         displayMode: this.displayMode,
@@ -88,29 +99,142 @@ export default class MyCalendarWebPart extends BaseClientSideWebPart<IMyCalendar
           },
           groups: [
             {
+              groupName: strings.behaviorPropertyGroupName,
+
               groupFields: [
-                this.propertyFieldNumber("refreshInterval", {
-                  key: "refreshInterval",
-                  // label: strings.RefreshInterval,
-                  label: strings.RefreshInterval,
+                // this.propertyFieldNumber("refreshInterval", {
+                //   key: "refreshInterval",
+                //   // label: strings.RefreshInterval,
+                //   label: strings.RefreshIntervalPropertyLabel,
+                //   value: this.properties.refreshInterval,
+                //   minValue: 1,
+                //   maxValue: 60
+                // }),
+                PropertyPaneSlider("refreshInterval", {                  
+                  label: strings.RefreshIntervalPropertyLabel,
                   value: this.properties.refreshInterval,
-                  minValue: 1,
-                  maxValue: 60
+                  min: 1,
+                  max: 60,
+                  step: 1
+
                 }),
                 PropertyPaneSlider('daysInAdvance', {
-                  label: strings.DaysInAdvance,
+                  label: strings.DaysInAdvancePropertyLabel,
                   min: 0,
                   max: 7,
                   step: 1,
                   value: this.properties.daysInAdvance
                 }),
                 PropertyPaneSlider('numMeetings', {
-                  label: strings.NumMeetings,
+                  label: strings.NumMeetingsPropertyLabel,
                   min: 0,
                   max: 20,
                   step: 1,
                   value: this.properties.numMeetings
+                }),
+                PropertyPaneDropdown("clickAction", {
+                  label: strings.ClickActionPropertyLabel,
+                  ariaLabel: strings.ClickActionPropertyLabel,
+                  selectedKey: this.properties.clickAction,                  
+                  options: [
+                    {                      
+                      text: ClickAction.Preview,
+                      key: ClickAction.Preview
+                    },                    
+                    {                     
+                      text: ClickAction.OpenInOutlook,
+                      key: ClickAction.OpenInOutlook
+                    }
+                  ]
                 })
+              ]
+            },            
+            {
+              groupName: strings.capabilitiesPropertyGroupName,
+              
+              groupFields: [                
+                
+                // PropertyPaneToggle("enableDelete", {
+                //   label: strings.EnableDeletePropertyLabel,
+                //   key: "enableDelete",
+                //   onText: strings.EnableOnPropertyText,
+                //   onAriaLabel: strings.EnableOnPropertyText,
+                //   offText: strings.EnableOffPropertyText,
+                //   offAriaLabel: strings.EnableOffPropertyText,
+                //   checked: this.properties.enableDelete
+                // }),
+                // PropertyPaneToggle("enableToggleReadStatus", {
+                //   label: strings.EnableToggleReadPropertyLabel,
+                //   key: "enableToggleReadStatus",
+                //   onText: strings.EnableOnPropertyText,
+                //   onAriaLabel: strings.EnableOnPropertyText,
+                //   offText: strings.EnableOffPropertyText,
+                //   offAriaLabel: strings.EnableOffPropertyText,
+                //   checked: this.properties.enableToggleReadStatus
+                // }),
+                // PropertyPaneToggle("enableToggleFlag", {
+                //   label: strings.EnableToggleFlagPropertyLabel,
+                //   key: "enableToggleFlag",
+                //   onText: strings.EnableOnPropertyText,
+                //   onAriaLabel: strings.EnableOnPropertyText,
+                //   offText: strings.EnableOffPropertyText,
+                //   offAriaLabel: strings.EnableOffPropertyText,
+                //   checked: this.properties.enableToggleFlag
+                // }),
+                PropertyPaneToggle("showNew", {
+                  label: strings.ShowNewPropertyLabel,
+                  key: "showNew",
+                  onText: strings.ShowOnPropertyText,
+                  onAriaLabel: strings.ShowOnPropertyText,
+                  offText: strings.ShowOffPropertyText,
+                  offAriaLabel: strings.ShowOffPropertyText,
+                  checked: this.properties.showNew
+                }),
+                PropertyPaneToggle("showViewAll", {
+                  label: strings.ShowViewAllPropertyLabel,
+                  key: "showViewAll",
+                  onText: strings.ShowOnPropertyText,
+                  onAriaLabel: strings.ShowOnPropertyText,
+                  offText: strings.ShowOffPropertyText,
+                  offAriaLabel: strings.ShowOffPropertyText,
+                  checked: this.properties.showViewAll
+                })
+              ],
+              
+            },
+            {
+              groupName: strings.uiPropertyGroupName,
+              
+              groupFields: [                
+                PropertyPaneDropdown("headerDisplay", {
+                  label: strings.HeaderDisplayPropertyLabel,
+                  ariaLabel: strings.HeaderDisplayPropertyLabel,
+                  selectedKey: this.properties.headerDisplay,                  
+                  options: [
+                    {                      
+                      text:  HeaderDisplay.Standard,
+                      key: HeaderDisplay.Standard
+                    },
+                    {                      
+                      text: HeaderDisplay.Large,
+                      key: HeaderDisplay.Large
+                    },
+                    {                     
+                      text: HeaderDisplay.None,
+                      key: HeaderDisplay.None
+                    }
+                  ]
+                })/*,
+              
+                PropertyPaneToggle("enableThemes", {
+                  label: strings.EnableThemesPropertyLabel,
+                  key: "enableThemes",
+                  onText: strings.EnableOnPropertyText,
+                  onAriaLabel: strings.EnableOnPropertyText,
+                  offText: strings.EnableOffPropertyText,
+                  offAriaLabel: strings.EnableOffPropertyText,
+                  checked: this.properties.enableThemes
+                })*/
               ]
             }
           ]
--- src/Events/myCalendar/src/webparts/myCalendar/loc/mystrings.d.ts
+++ src/Events/myCalendar/src/webparts/myCalendar/loc/mystrings.d.ts
@@ -1,18 +1,32 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 declare interface IMyCalendarWebPartStrings {
-  AllDay: string;
-  DaysInAdvance: string;
+  PropertyPaneDescription: string;  
+  HeaderDisplayPropertyLabel: string;
+  MessagesToShowPropertyLabel: string;
+  RefreshIntervalPropertyLabel:string; 
+  DaysInAdvancePropertyLabel: string; 
+  NumMeetingsPropertyLabel: string;
+  ClickActionPropertyLabel: string;
+  ShowNewPropertyLabel: string;  
+  ShowViewAllPropertyLabel: string;  
+  EnableThemesPropertyLabel: string;
+  ShowOnPropertyText: string;
+  ShowOffPropertyText: string;
+  EnableOnPropertyText: string;
+  EnableOffPropertyText: string;
+  behaviorPropertyGroupName: string;
+  capabilitiesPropertyGroupName: string;
+  uiPropertyGroupName: string;
+  
+  AllDay: string;  
   Error: string;
   Hour: string;
   Hours: string;
   Loading: string;
   Minutes: string;
   NewMeeting: string;
-  NoMeetings: string;
-  NumMeetings: string;
-  RefreshInterval: string;
-  PropertyPaneDescription: string;
+  NoMeetings: string;    
   ViewAll: string;
 }
 
